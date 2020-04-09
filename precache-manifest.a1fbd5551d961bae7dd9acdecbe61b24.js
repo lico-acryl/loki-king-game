9ce542f3bc05d2c8d63dad464a3a536aca7eb4b9
lico-acryl <lico@acriil.com>
2020-04-09 18:19:29 +0900

    Updates

---
+++ precache-manifest.a1fbd5551d961bae7dd9acdecbe61b24.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c075c74e68a8b290698e3c49a238ad",
+    "url": "/loki-king-game/index.html"
+  },
+  {
+    "revision": "5d9f095c43de988bf489",
+    "url": "/loki-king-game/static/css/main.e4d5c891.chunk.css"
+  },
+  {
+    "revision": "020f29699b7345582893",
+    "url": "/loki-king-game/static/js/2.534fd172.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/loki-king-game/static/js/2.534fd172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d9f095c43de988bf489",
+    "url": "/loki-king-game/static/js/main.5dfda340.chunk.js"
+  },
+  {
+    "revision": "c8edba7ecb32e010228d",
+    "url": "/loki-king-game/static/js/runtime-main.30f787ff.js"
+  },
+  {
+    "revision": "57f2781598b5b3108103e699cc322b78",
+    "url": "/loki-king-game/static/media/lico.57f27815.png"
+  },
+  {
+    "revision": "86f10598887150f6afc6b337daeac9b4",
+    "url": "/loki-king-game/static/media/loki.86f10598.png"
+  },
+  {
+    "revision": "543f17ba2387ca6ea0fe1f69f50fb304",
+    "url": "/loki-king-game/static/media/trevor.543f17ba.png"
+  }
+]);
